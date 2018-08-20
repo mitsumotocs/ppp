fc883749dc409e1e2c7c600185cd6cc36b622d4a
mitsumotocs <mitsumotocs@gmail.com>
2018-08-20 09:51:43 +0900

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ppp
+# PPP - Primitive PHP Project
+
+Pretty much all I need to build small web apps.
---
+++ src/Config/Config.php
@@ -0,0 +1,51 @@
+<?php
+namespace PPP\Config;
+
+class Config
+{
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        if (isset($this->$name)) {
+            return $this->$name;
+        } else {
+            throw new \LogicException(sprintf('Value named "%s" is not defined.', $name));
+        }
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     * @return void
+     */
+    public function __set($name, $value)
+    {
+        throw new \LogicException(sprintf('You cannot set a value to an instance of %s.', __CLASS__));
+    }
+
+    /**
+     * @param $name
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        $value = $this->get($name);
+        return isset($value);
+    }
+
+    /**
+     * @param string|null $name
+     * @return mixed|array
+     */
+    public function get($name = null)
+    {
+        if (isset($name)) {
+            return $this->__get($name);
+        } else {
+            return get_object_vars($this);
+        }
+    }
+}
